e19decb9f3a91bb6b96be274a932f012a5ef0d5d
vSpaike <stryckeurs75@gmail.com>
2025-06-25 22:22:07 +0200

    add: function to_bst but not finish sorted

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Il faut s'assurer d'être dans la racine, et il faut enter la commande dans le t
  - python -m unittest tests/test_generate.py
  - python -m unittest tests/test_split_conseq.py
  - python -m unittest tests/test_bst.py
+## Arbre binaire de recherche
+For the method smallest i know there is a possibility to a have a coplexity of O(1) like the method biggest but i don't know how to write it. I spent to many time et try to many things so i decided to write it in another way with a little of complexity  
--- binary_search_tree/bst.py
+++ binary_search_tree/bst.py
@@ -1,18 +1,37 @@
+import math
 
 class BinarySearchTree: 
 
     def __init__(self,array):
         self.bst=array
+        self.size=len(array)
 
-
-    def to_bst(self,array):
-        if len(array)==0:
+    def to_bst(self):
+        if not len(self.bst):
             return []
-        #TO FINISH
+        self.bst.sort()
+        final = self.bst.copy()
+
+        def create_rec_bst(start, index, end):
+            if start>end or index>=self.size:
+                return None
+            
+            middle = (start+end)//2
+            final[index] = self.bst[middle]
+
+            #left part
+            create_rec_bst(start, 2*index+1, middle - 1)
+            
+            #right part
+            create_rec_bst(middle + 1, 2*index+2, end)
+
+        create_rec_bst(0, 0,self.size-1)
+        return final
+        
 
 
     def valid(self):
-        for i in range(len(self.bst)//2):
+        for i in range(self.size//2):
             if((self.bst[2*i + 1] != None) and (self.bst[i] < self.bst[2*i + 1])) or ((self.bst[2*i + 2] != None) and (self.bst[i] > self.bst[2*i + 2])):
                 return False
         return True
@@ -21,39 +40,52 @@ class BinarySearchTree:
     def exists(self,value):
         current_index = 0
         
-        while (current_index < len(self.bst)):
+        while (current_index < self.size):
             if value > self.bst[current_index]:
                 current_index= current_index *2+2
             elif value < self.bst[current_index]:
                 current_index= current_index *2+1
             else:
                 return True
-        
         return False
 
 
     def smallest(self):
-        if self.bst[len(self.bst)//2] == None:
-            return self.bst[(len(self.bst)//2-1)//2]
-        if (len(self.bst)+1)%2 == 0:
-            return self.bst[(len(self.bst)+1)//2]
-        return self.bst[len(self.bst)//2]
-
+        size= int(math.log2(self.size))+1
+        if size==2:
+            size-=1
+        current = self.bst[0]
+        for i in range(size):
+            if self.bst[2*i+1] != None:
+                current = self.bst[2*i+1]  
+        return current
 
     def biggest(self):
         if(self.bst[-1] == None):
-            return self.bst[((len(self.bst)-1)//2)-1]
+            return self.bst[((self.size-1)//2)-1]
         return self.bst[-1]
 
 
-    def sorted_array(self): 
-        final= []
-        current=[self.bst[0]]
-        index=0
-        for i in range (len(self.bst)//2):
-            pass
+    def sorted_array(self):
+        if not len(self.bst):
+            return []
+        
+        final=self.bst.copy()
+        def sorted_rec(start, index, end):
+            if start>end or index>=self.size:
+                return None
+            
+            middle = (start+end)//2
+            final[index] = self.bst[middle]
+
+            #left part
+            sorted_rec(start, 2*index+1, middle - 1)
+            
+            #right part
+            sorted_rec(middle + 1, 2*index+2, end)
+
 
 
 if __name__ == "__main__":
-    a= BinarySearchTree([6,3,7])
-    print(a.exists(98))
+    a= BinarySearchTree([2,3,4,6,7,8,9])
+    print(a.to_bst())
